c988df94eab26bbceb787439730b5cb498ca0126
Ridwan Adewole <devridwan1@gmail.com>
2024-02-02 21:29:49 +0000

    designed responsive nav bar and hero section

---
+++ index.js
@@ -0,0 +1,16 @@
+const menuBtn = document.querySelector('.menu-toggle')
+const menu = document.querySelector('.menu')
+
+menuBtn.addEventListener('click', handleOpenMenu)
+
+function handleOpenMenu() {
+  menuBtn.classList.toggle('active')
+
+  if (menuBtn.classList.contains('active')) {
+    menu.style.right = 0
+    menu.style.transition = '1s ease-in-out'
+  } else {
+    menu.style.right = '-1000%'
+    menu.style.transition = '1s ease-in-out'
+  }
+}
